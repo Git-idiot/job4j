3130f6c7feade276285c09fba9f9e4a959e707af
Git-idiot <pesterevamb@gmail.com>
2019-02-07 00:25:52 +0300

    checkstyle

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -11,15 +11,13 @@ public class Calculate {
      * Main.
      * @param args - args.
      */
-	public static void main (String[] args){
-		System.out.println("Hello world");
+	public static void main(String[] args) {System.out.println("Hello world");
 	}
 	/**
      * Method echo.
      * @param name Your name.
      * @return Echo plus your name.
      */
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
+    public String echo(String name) {return "Echo, echo, echo : " + name;
     }
 }
