7080745010ca8257b20be041a410edae45d681e7
Git-idiot <pesterevamb@gmail.com>
2019-02-28 22:07:12 +0300

    Point

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -14,18 +14,30 @@ public class Point {
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+       // System.out.println(this);
     }
 
     public double distanceTo(Point that) {
+        System.out.println(this);
+        System.out.println(that);
         return Math.sqrt(
                 Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
         );
     }
+
     public static void main(String[] args) {
         Point a = new Point(1,1);
         Point b = new Point(2,2);
         double res= a.distanceTo(b);
         System.out.println(res);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
 
