035e768369417247546373b4a46c34009d49d96b
Git-idiot <pesterevamb@gmail.com>
2019-03-18 23:02:22 +0300

    Find element in Array

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,4 +1,29 @@
 package ru.job4j.array;
-
+/**
+ * поиск элемента в массиве и вывод его номера
+ **/
 public class FindLoop {
+    /**
+     * Возводим в квадрат и заполняем массив.
+     * @param data массив.
+     * @param el искомый элемент
+     * @return номер элемента массива.
+     */
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        FindLoop findLoop = new FindLoop();
+        int[] array = {5, 2, 1, 7, 9};
+        int result = findLoop.indexOf(array, 3);
+        System.out.println(result);
+    }
 }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,4 +1,32 @@
 package ru.job4j.array;
 
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class FindLoopTest {
+    /**
+     * Поиск элемента в массиве. Элемент в массиве присутствует
+     */
+    @Test
+    public void elementExist(){
+        FindLoop findLoop = new FindLoop();
+        int[] array = {5, 2, 3, 7, 9};
+        int result = findLoop.indexOf(array, 3);
+        int expected = 2;
+        assertThat(result, is(expected));
+    }
+    /**
+     * Поиск элемента в массиве. Элемента в массиве нет
+     */
+    @Test
+    public void elementNotExist(){
+        FindLoop findLoop = new FindLoop();
+        int[] array = {5, 2, 2, 7, 9};
+        int result = findLoop.indexOf(array, 3);
+        int expected = -1;
+        assertThat(result, is(expected));
+    }
+
+
 }
