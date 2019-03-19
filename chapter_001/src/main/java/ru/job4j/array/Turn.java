8c994fc56c4320ab9282a336b1de24c408dfbb3f
Git-idiot <pesterevamb@gmail.com>
2019-03-19 23:44:00 +0300

    Переворот массива

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -2,19 +2,10 @@ package ru.job4j.array;
 
 public class Turn {
     public int[] turnArray (int[] array) {
-        //int tmp = -1;
-        int last = array.length-1;
         for (int i = 0; i < array.length/2; i++){
-//            int tmp = array[i];
-//            array[i] = array[array.length-1];
-//            array[array.length-1] = tmp;
-            //int last = array.length;
             int tmp = array[i];
             array[i] = array[array.length-i-1];
             array[array.length-i-1] = tmp;
-            //last--;
-            //System.out.println(array[i]);
-
         }
         return array;
     }
