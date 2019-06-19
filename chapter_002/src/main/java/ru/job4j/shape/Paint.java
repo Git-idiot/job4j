9def96c2b0dbce6082bc860054b0776b1cba2eb2
Git-idiot <pesterevamb@gmail.com>
2019-06-19 23:52:34 +0300

    Strategy, tests

--- chapter_002/src/main/java/ru/job4j/shape/Paint.java
+++ chapter_002/src/main/java/ru/job4j/shape/Paint.java
@@ -1,11 +1,20 @@
 package ru.job4j.shape;
 
 public class Paint {
+    private  ShapeStrategy shapeStrategy;
+
+
+    public String drawShape(ShapeStrategy shapeStrategy){
+        shapeStrategy.draw();
+        return shapeStrategy.draw();
+    }
 
     public static void main(String[] args) {
-        Context context = new Context(new Triangle());
-        System.out.println(context.executeStrategy());
-        context = new Context(new Square());
-        System.out.println(context.executeStrategy());
+        Paint paint = new Paint();
+        System.out.println(paint.drawShape(new Square()));
+
+
+
     }
+
 }
